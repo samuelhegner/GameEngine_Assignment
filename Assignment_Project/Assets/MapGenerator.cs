5ad549f6ebedf9f6d06f062a476d0e63cfe634fe
samuelhegner <C16439262@mydit.ie>
2018-11-23 23:32:50 +0000

    Live Editor Noise Map Texture

--- Assignment_Project/Assets/MapGenerator.cs
+++ Assignment_Project/Assets/MapGenerator.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
+	public int mapWidth;
+	public int mapHeight;
+	public float noiseScale;
+	public bool autoUpdate;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void GenerateMap(){
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+		MapDisplay display = FindObjectOfType<MapDisplay>();
+		display.DrawNoiseMap(noiseMap);
 	}
 }
