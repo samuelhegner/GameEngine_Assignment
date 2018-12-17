d5d54af3f6b2d906c6b94eae80457969480df9fa
samuelhegner <C16439262@mydit.ie>
2018-12-17 21:23:30 +0000

    Cloud Spawner Start

---
+++ Assignment_Project/Assets/Spawn_Clouds.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Clouds : MonoBehaviour
+{   
+    public GameObject prefab;
+
+    public float spawnRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnCloud());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnCloud(){
+        while(true){
+            Vector3 spawnPoint = new Vector3(Random.Range(-1000, 1000), 0, 0);
+
+            Vector3 localSpawn = transform.TransformPoint(spawnPoint);
+
+            Instantiate(prefab, localSpawn, transform.rotation, transform);
+            yield return new WaitForSeconds(spawnRate);
+        }
+
+    }
+}
