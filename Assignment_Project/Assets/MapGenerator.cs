26ee066dc03144fc81941cd93fcdf24981cc37e1
samuelhegner <C16439262@mydit.ie>
2018-12-19 17:26:04 +0000

    Tree Placement Start

--- Assignment_Project/Assets/MapGenerator.cs
+++ Assignment_Project/Assets/MapGenerator.cs
@@ -83,21 +83,66 @@ public class MapGenerator : MonoBehaviour {
 	}
 
 	void Awake(){
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
 		if(randomOnStart){
 			seed = Random.Range(-1000, 1000);
 			offset = new Vector2(Random.Range(-100, 100), Random.Range(-100, 100));
 		}
 		GenerateMap();
 		pools = Object_Pool.Instance;
+
+		//set region range
+		/* for(int i = 0; i < regions.Length; i++){
+			if(i == 0){
+				regions[i].minRange = 0;
+				regions[i].maxRange = meshHeightMultiplier * meshHeightCurve.Evaluate(regions[i].height);
+			}else if(i < regions.Length){
+				regions[i].minRange = meshHeightMultiplier * meshHeightCurve.Evaluate(regions[i-1].height);
+				regions[i].maxRange = meshHeightCurve.Evaluate(regions[i].height) * meshHeightMultiplier;
+			}
+		}*/
 	}
 
 	void Start(){
 
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
 		// Tree spawning saved for a later date
 
 		Mesh mesh = GameObject.Find("Mesh").GetComponent<MeshFilter>().sharedMesh;
 		pools.SpawnFromPool("Tree", mesh.vertices[0] * 10f, Quaternion.identity);
+		
+		int treeRegions = 0;
+
+		for(int i = 0; i < regions.Length; i++){
+			if(regions[i].trees){
+				treeRegions++;
+			}
+		}
+		float dividedTreeCount = numberOfTrees/treeRegions;
+
+		for(int y = 0; y < mapChunkSize; y ++){
+            for(int x = 0; x < mapChunkSize; x++){
+				for(int i = 0; i < regions.Length; i++){
+					
+					if(i == 0){
+
+					}else{
+						if(regions[i].trees){
+							if(noiseMap[x,y] < regions[i].height && noiseMap[x,y] > regions[i-1].height){
+								
+								float ran = Random.Range(0, 10);
+								if(ran < 1){
+									pools.SpawnFromPool("Tree", mesh.vertices[((y * mapChunkSize)+x)] * 10, Quaternion.identity);
+								}
+
+							}
+						}
+					}
+				}
+            }
+        }
 
 
 		//float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
@@ -121,4 +166,7 @@ public struct TerrainType
 	public Color colour;
 
 	public bool trees;
+
+	public float minRange;
+	public float maxRange;
 }
